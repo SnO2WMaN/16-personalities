4cfee97fed2ad45683de9135b073d9782efedede
SnO₂WMaN <me@sno2wman.net>
2024-05-27 21:25:00 +0900

    Recreate (#7)
    * アー
    
    * fx
    
    * fuckci
    
    * fuck
    
    * fuck

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default
+
+{
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        twitter: {
+          1: "#1da1f2",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
